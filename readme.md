e65b7fb677a9556ffef90eaf94192994a2bf8918
devjanaprime <dev@primeacademy.io>
2017-04-12 00:00:36 -0500

    first

---
+++ readme.md
@@ -0,0 +1,16 @@
+Cards Lecture 3
+---------------
+
+Using what we've learned about JS and JQ, we'll create a cards app that will do the following:
+
+- drawCardFromTop: first card in array
+- drawCardFromBottom: last card in array
+- drawRandomCard: get a random index in the deck and show on DOM
+- showDeck: displays the full deck in order on the screen
+- shuffleDeck: randomizes the deck
+- hideAll: clears the DOM
+- click on a card and it will tell you on what card you clicked
+
+We'll start with some basic HTML, CSS, and JS. I've included a starting framework including an array of cards, some basic styles, and some images to approximate cards.
+
+We'll do a lecture, then you'll have time to recreate. Then we'll explore a bit more as a class.
---
+++ scripts/cards3.js
@@ -0,0 +1,84 @@
+var deck = [
+  { number: 1, suit: 'spades' },
+  { number: 2, suit: 'spades' },
+  { number: 3, suit: 'spades' },
+  { number: 4, suit: 'spades' },
+  { number: 5, suit: 'spades' },
+  { number: 6, suit: 'spades' },
+  { number: 7, suit: 'spades' },
+  { number: 8, suit: 'spades' },
+  { number: 9, suit: 'spades' },
+  { number: 10, suit: 'spades' },
+  { number: 11, suit: 'spades' },
+  { number: 12, suit: 'spades' },
+  { number: 13, suit: 'spades' },
+  { number: 1, suit: 'diamonds' },
+  { number: 2, suit: 'diamonds' },
+  { number: 3, suit: 'diamonds' },
+  { number: 4, suit: 'diamonds' },
+  { number: 5, suit: 'diamonds' },
+  { number: 6, suit: 'diamonds' },
+  { number: 7, suit: 'diamonds' },
+  { number: 8, suit: 'diamonds' },
+  { number: 9, suit: 'diamonds' },
+  { number: 10, suit: 'diamonds' },
+  { number: 11, suit: 'diamonds' },
+  { number: 12, suit: 'diamonds' },
+  { number: 13, suit: 'diamonds' },
+  { number: 1, suit: 'hearts' },
+  { number: 2, suit: 'hearts' },
+  { number: 3, suit: 'hearts' },
+  { number: 4, suit: 'hearts' },
+  { number: 5, suit: 'hearts' },
+  { number: 6, suit: 'hearts' },
+  { number: 7, suit: 'hearts' },
+  { number: 8, suit: 'hearts' },
+  { number: 9, suit: 'hearts' },
+  { number: 10, suit: 'hearts' },
+  { number: 11, suit: 'hearts' },
+  { number: 12, suit: 'hearts' },
+  { number: 13, suit: 'hearts' },
+  { number: 1, suit: 'clubs' },
+  { number: 2, suit: 'clubs' },
+  { number: 3, suit: 'clubs' },
+  { number: 4, suit: 'clubs' },
+  { number: 5, suit: 'clubs' },
+  { number: 6, suit: 'clubs' },
+  { number: 7, suit: 'clubs' },
+  { number: 8, suit: 'clubs' },
+  { number: 9, suit: 'clubs' },
+  { number: 10, suit: 'clubs' },
+  { number: 11, suit: 'clubs' },
+  { number: 12, suit: 'clubs' },
+  { number: 13, suit: 'clubs' }
+]
+
+$( document ).ready( function(){
+  $( '.card' ).on( 'click', function(){
+    // when we click on a card we'll want to know on what card we clicked
+  }); // end card class on click
+
+  $( '#drawCardFromBottom' ).on( 'click', function(){
+    console.log( 'in drawCardFromBottom');
+  }); // end drawCardFromBottom
+
+  $( '#drawCardFromTop' ).on( 'click', function(){
+    console.log( 'in drawCardFromTop');
+  }); // end drawCardFromTop
+
+  $( '#drawRandomCard' ).on( 'click', function(){
+    console.log( 'in drawRandomCard');
+  }); // end drawRandomCard
+
+  $( '#hideAll' ).on( 'click', function(){
+    console.log( 'in hideAll');
+  }); // end hideAll
+
+  $( '#showDeck' ).on( 'click', function(){
+    console.log( 'in showDeck');
+  }); // end showDeck
+
+  $( '#shuffleDeck' ).on( 'click', function(){
+    console.log( 'in shuffleDeck');
+  }); // end shuffleDeck
+}); // end doc  ready
